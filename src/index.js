40cb56cac72afff96c24706720b4f398d7214147
pedrofracassi <pedroluizfracassi@gmail.com>
2020-09-20 19:03:34 -0300

    Fix environment variable typo

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 import App from './App';
 
 Sentry.init({
-  dsn: process.env.REACt_APP_SENTRY_DSN,
+  dsn: process.env.REACT_APP_SENTRY_DSN,
   integrations: [
     new Integrations.BrowserTracing(),
   ],
